23a117c9af9511ea0851717bcdf694b420486324
DinushiWanniarachchi <dinushinwanniarachchi@gmail.com>
2024-03-25 10:01:39 +0530

    Update EmailSection.jsx

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,11 +1,4 @@
-import GithubIcon from "../../../public/github-icon.svg";
-import LinkedinIcon from "../../../public/linkedin-icon.svg";
-import Link from "next/link";
-import Image from "next/image";
-
-
-        
-          
+            
 const EmailSection = () => {
   return (
     <section
