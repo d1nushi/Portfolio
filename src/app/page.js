6a07bd082eafdcef665a17084314cb1a4b92e32e
DinushiWanniarachchi <dinushinwanniarachchi@gmail.com>
2024-03-25 10:10:45 +0530

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
-import EmailSection from "./components/EmailSection";
+
 import Footer from "./components/Footer";
 
 export default function Home() {
@@ -13,7 +13,7 @@ export default function Home() {
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
-        <EmailSection />
+        
       </div>
       <Footer />
     </main>
